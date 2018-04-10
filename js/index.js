f8e39e285cfdc99aeb15edf334da8110a46a7ca5
Marat Valiev <mz.valiev@gmail.com>
2018-04-10 08:24:18 -0400

    Add Pomodoro project to github

---
+++ js/index.js
@@ -0,0 +1,160 @@
+//Break buttons
+const breakAddTimeButton = $('#breakAdd');
+const breakRemoveTimeButton = $('#breakRemove');
+//Session buttons
+const sessionAddTimeButton = $('#sessionAdd');
+const sessionRemoveTimeButton = $('#sessionRemove');
+//Break set time display
+const breakSetDisplay = $('#breakSet');
+//Session set time display
+const sessionSetDisplay = $('#sessionSet');
+//Session/break timer
+const sessionTimeDisplay = $('#sessionTime');
+const statusText = $('#statusText');
+//Buttons
+const startButton = $('#start');
+const pauseButton = $('#pause');
+const resetButton = $('#reset');
+let breakMin = 5;
+let breakSec = 0;
+let sessionMin = 25;
+let sessionSec = 0;
+let currMin = sessionMin;
+let currSec = sessionSec;
+let isRunning = false;
+let intID;
+let session = true;
+
+$(function () {
+    breakAddTimeButton.on('click', function (e) {
+        e.preventDefault();
+        if (!isRunning) {
+            let curr = parseInt(breakSetDisplay.text());
+            curr++;
+            breakMin = curr;
+            breakSetDisplay.text(curr);
+        }
+    });
+    breakRemoveTimeButton.on('click', function (e) {
+        e.preventDefault();
+        if (!isRunning) {
+            let curr = parseInt(breakSetDisplay.text());
+            if (curr > 1) {
+                curr--;
+            }
+            breakMin = curr;
+            breakSetDisplay.text(curr);
+        }
+    });
+
+    sessionAddTimeButton.on('click', function (e) {
+        e.preventDefault();
+        if (!isRunning) {
+            let curr = parseInt(sessionSetDisplay.text());
+            curr++;
+            sessionSetDisplay.text(curr);
+            sessionTimeDisplay.text(curr);
+            sessionMin = curr;
+            reset();
+        }
+    });
+
+    sessionRemoveTimeButton.on('click', function (e) {
+        e.preventDefault();
+        if (!isRunning) {
+            let curr = parseInt(sessionSetDisplay.text());
+            if (curr > 1) {
+                curr--;
+            }
+            sessionSetDisplay.text(curr);
+            sessionTimeDisplay.text(curr);
+            sessionMin = curr;
+            reset();
+        }
+    });
+
+    function startTimer() {
+        //Disable start button after click
+        startButton.prop('disabled', true);
+        let min = currMin;
+        let sec = currSec;
+        intID = setInterval(function () {
+
+            if (sec > 0) {
+                sec--;
+                if (sec < 10) {
+                    $('#sec').text('0' + sec);
+                } else {
+                    $('#sec').text(sec);
+                }
+            } else {
+                min--;
+                sec = 59;
+                $('#sec').text(sec);
+                $('#min').text(min);
+            }
+
+            if (min === 0 && sec === 0) {
+                if (session) {
+                    statusText.text('Break');
+                    session = false;
+                    min = breakMin;
+                    sec = breakSec;
+                } else {
+                    statusText.text('Session');
+                    session = true;
+                    min = sessionMin;
+                    sec = sessionSec;
+                }
+            }
+        }, 1000);
+    }
+
+    function stopTimer() {
+        clearInterval(intID);
+        currMin = parseInt($('#min').text());
+        currSec = parseInt($('#sec').text());
+        startButton.prop('disabled', false);
+    }
+
+    function reset() {
+        if (isRunning) {
+            stopTimer();
+            isRunning = false;
+        }
+        currSec = 0;
+        currMin = sessionMin;
+        session = true;
+        $('#sec').text('00');
+        $('#min').text(currMin);
+        startButton.prop('disabled', false);
+    }
+    startButton.on('click', function (e) {
+        e.preventDefault();
+        startTimer();
+        isRunning = true;
+        pauseButton.removeClass('active');
+        startButton.addClass(' active');
+    });
+
+    pauseButton.on('click', function (e) {
+        e.preventDefault();
+        if (isRunning) {
+            stopTimer();
+            isRunning = false;
+        }
+        startButton.removeClass('active');
+        pauseButton.addClass(' active');
+    });
+    
+    resetButton.on('click', function (e) {
+        e.preventDefault();
+        if (isRunning) {
+            reset();
+        } else if (!isRunning) {
+            reset();
+        }
+        pauseButton.removeClass('active');
+        startButton.removeClass('active');
+    });
+});
